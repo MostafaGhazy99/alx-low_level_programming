169e69f151f1809f4e496047bf734a4963fc13eb
MostafaGhazy99 <mostafagahzi616@gmail.com>
2023-06-20 04:57:22 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,9 +6,7 @@ void function1(void);
 int function2(int x, int y);
 int _putchar(char c);
 void print_alphabet(void);
-
-
-
+int _putchar(char c);
+void print_alphabet_x10(void);
 /* Add more function prototypes as needed */
-
 #endif /* MAIN_H */
