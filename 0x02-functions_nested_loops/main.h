6a1b8936df611c0adf6a518c31f849cacbf62a82
MostafaGhazy99 <mostafagahzi616@gmail.com>
2023-06-21 04:02:27 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,5 +20,20 @@ int print_sign(int n);
 
 int _putchar(char c);
 int _abs(int n);
+
+int _putchar(char c);
+int print_last_digit(int n);
+
+int _putchar(char c);
+void jack_bauer(void);
+
+int _putchar(char c);
+void times_table(void);
+
+int _putchar(char c);
+int add(int a, int b);
+
+int _putchar(char c);
+void print_to_98(int n);
 /* Add more function prototypes as needed */
 #endif /* MAIN_H */
