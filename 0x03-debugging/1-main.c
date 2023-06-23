3f7249bdf2f1a30307ca5dcc9746db4b7f84ceff
MostafaGhazy99 <mostafagahzi616@gmail.com>
2023-06-23 09:32:35 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,15 +1,23 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - causes an infinite loop
+ * Return: 0
  */
 int main(void)
 {
-	int n;
+	int i;
 
-	n = 0;
-	positive_or_negative(n);
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
