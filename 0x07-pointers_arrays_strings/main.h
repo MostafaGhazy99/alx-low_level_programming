76b9930bface76e8513929ae88a5a065503277df
MostafaGhazy99 <mostafagahzi616@gmail.com>
2023-07-03 12:00:53 -0700

    ./8-print_diagsums

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,4 +12,16 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, int n);
+char *_strncpy(char *dest, const char *src, int n);
+int _strcmp(const char *s1, const char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+
 #endif /* MAIN_H */
