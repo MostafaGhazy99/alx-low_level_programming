1459d3bcfecd119755f3b85fd8154ee3f5413e79
MostafaGhazy99 <mostafagahzi616@gmail.com>
2023-07-03 11:56:13 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
@@ -9,5 +11,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+void print_chessboard(char (*a)[8]);
 #endif /* MAIN_H */
