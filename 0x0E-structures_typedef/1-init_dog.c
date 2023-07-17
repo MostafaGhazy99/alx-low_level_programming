bc97bb16d8621a14fde9ba4e8f90e92870bb81ba
MostafaGhazy99 <mostafagahzi616@gmail.com>
2023-07-17 16:42:57 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-init_dog.c -o b

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stddef.h> 
+
+/* Add this line for NULL declaration */
+/**
+ * init_dog - Initializes a struct dog
+ * @d: Pointer to the struct dog to initialize
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to the owner of the dog
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - Structure representing a dog
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to the owner of the dog
+ *
+ * Description: This structure represents a dog
+ */
+struct dog
+{
 char *name;
 float age;
 char *owner;
