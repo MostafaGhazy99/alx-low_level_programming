843d4687698ed7fec5e72c11284bb3d8a2d974a0
MostafaGhazy99 <mostafagahzi616@gmail.com>
2023-07-17 16:14:17 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+int add_numbers(int a, int b);
+void print_message(char *message);
+
+/* Macro definitions */
+#define MAX_SIZE 100
+
+/* Global variable declarations */
+extern int global_variable;
+
+#endif /* MAIN_H */
