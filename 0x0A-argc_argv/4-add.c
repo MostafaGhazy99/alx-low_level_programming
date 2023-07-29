629adac232a611508619b8f0bcbb9fcd386a3d70
MostafaGhazy99 <mostafagahzi616@gmail.com>
2023-07-29 16:11:46 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - Entry point
@@ -9,6 +10,23 @@
  *
  * Return: 0 if successful, 1 if error
  */
+
+int is_positive_number(const char *str) 
+{
+if (str == NULL || *str == '\0') 
+{
+return (0);
+}
+for (int i = 0; str[i] != '\0'; i++) 
+{
+if (!isdigit(str[i]) && str[i] != '-') 
+{
+return (0);
+}
+}
+return (1);
+}
+
 int main(int argc, char *argv[])
 {
 int i, num, sum = 0;
