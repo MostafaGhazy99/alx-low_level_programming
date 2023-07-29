defb53ccbb7dbb99d4df64501b49d32b30b78cb5
MostafaGhazy99 <mostafagahzi616@gmail.com>
2023-07-29 15:43:37 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,10 +10,9 @@
 int main(int argc, char *argv[])
 {
 int i;
-
 for (i = 0; i < argc; i++)
 {
-puts(argv[i]);
+printf("%s", argv[i]);
 putchar('\n');
 }
 return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,5 +4,6 @@
 int _putchar(char c);
 void _puts(char *str);
 int atoi(const char *s);
+void print_arguments(int argc, char *argv[]);
 
 #endif /* MAIN_H */
